34aec556caa4bb6dd6e5b586d404d43a3951c856
Garrett-B-P <77997734+Garrett-B-P@users.noreply.github.com>
2021-01-30 19:49:01 -0600

    Update Main.java

--- src/mcmurray/Main.java
+++ src/mcmurray/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Braeden McMurray.");
         System.out.println("Hello, Braeden, my name is Cajsa.");
+        System.out.println("Hello, Braeden, my name is Garrett.")
     }
 }
